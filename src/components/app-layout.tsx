37d7f712394b0a08fad9d7d07b65d5569c5bfd27
Emmanuel TUYISHIMIRE <emmanueltuyishimire766@gmail.com>
2025-10-19 17:23:44 +0000

    Try fixing this error: `Console Error: <a> cannot contain a nested <a>.

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -63,7 +63,7 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
           <NavigationMenuList>
              {navItems.map((item) => (
               <NavigationMenuItem key={item.href}>
-                <Link href={item.href} passHref>
+                <Link href={item.href} legacyBehavior passHref>
                   <NavigationMenuLink active={pathname.startsWith(item.href)} className={navigationMenuTriggerStyle()}>
                     {item.label}
                   </NavigationMenuLink>
@@ -90,7 +90,7 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
                 href="/"
                 className="flex items-center gap-2 text-lg font-semibold"
               >
-                <Image src="/logo.png" alt="Math Calculators Logo" width={56} height={56} />
+                <Image src="/logo.png" alt="Math Calculators Logo" width={56} height={56} className="dark:bg-white dark:p-1 dark:rounded-sm" />
                 <span>Math Calculators</span>
               </Link>
               {navItems.map((item) => (
