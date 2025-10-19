0db6a866f4f9348a1728acc3e632eb61b2f7e3fe
Emmanuel TUYISHIMIRE <emmanueltuyishimire766@gmail.com>
2025-10-19 15:50:53 +0000

    add site map navigation link to footer links and add black box border fo

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -40,6 +40,7 @@ export function AppFooter() {
                             <li><Link href="/privacy-policy" className="text-muted-foreground hover:text-primary">Privacy Policy</Link></li>
                             <li><Link href="/terms-of-service" className="text-muted-foreground hover:text-primary">Terms of Service</Link></li>
                             <li><Link href="/disclaimer" className="text-muted-foreground hover:text-primary">Disclaimer</Link></li>
+                            <li><Link href="/sitemap.xml" className="text-muted-foreground hover:text-primary">Sitemap</Link></li>
                         </ul>
                     </div>
                 </div>
