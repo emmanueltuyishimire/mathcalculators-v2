fda2e3440ea3f374c5022ffc9328bc1d34bab70b
Emmanuel TUYISHIMIRE <emmanueltuyishimire766@gmail.com>
2025-10-19 05:29:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5398649656-398ca",
+  "appId": "1:558032123487:web:47872311f8d5284d45cdea",
+  "apiKey": "AIzaSyBSyk9QgkJVpU-fcZXFW3Dscq13kAvAHRQ",
+  "authDomain": "studio-5398649656-398ca.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "558032123487"
+};
