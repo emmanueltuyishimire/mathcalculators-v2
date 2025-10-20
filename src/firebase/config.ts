218885119f2d2d622328b74bae747a6860a7698d
Emmanuel TUYISHIMIRE <emmanueltuyishimire766@gmail.com>
2025-10-20 13:15:28 +0000

    Ah! This error is happening because your Next.js app is trying to read t

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,20 +1,18 @@
-
-
 // This function will be called on the client side, where process.env is available.
 export const getFirebaseConfig = () => {
-    const apiKey = process.env.NEXT_PUBLIC_FIREBASE_API_KEY;
-    if (!apiKey) {
+    if (!process.env.NEXT_PUBLIC_FIREBASE_API_KEY) {
         // This error will be caught by the developer in their local environment
         // if the .env.local file is missing or the variable is not set.
         throw new Error("NEXT_PUBLIC_FIREBASE_API_KEY is not set in the environment.");
     }
 
     return {
-        "projectId": "studio-5398649656-398ca",
-        "appId": "1:558032123487:web:47872311f8d5284d45cdea",
-        "apiKey": apiKey,
-        "authDomain": "studio-5398649656-398ca.firebaseapp.com",
-        "measurementId": "",
-        "messagingSenderId": "558032123487"
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+        measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
     };
 };
