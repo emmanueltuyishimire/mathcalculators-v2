8a1ae58ca1368335ab7173a68c6654c1985486ef
Emmanuel TUYISHIMIRE <emmanueltuyishimire766@gmail.com>
2025-10-20 12:59:38 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,20 @@
-export const firebaseConfig = {
-  "projectId": "studio-5398649656-398ca",
-  "appId": "1:558032123487:web:47872311f8d5284d45cdea",
-  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  "authDomain": "studio-5398649656-398ca.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "558032123487"
+
+
+// This function will be called on the client side, where process.env is available.
+export const getFirebaseConfig = () => {
+    const apiKey = process.env.NEXT_PUBLIC_FIREBASE_API_KEY;
+    if (!apiKey) {
+        // This error will be caught by the developer in their local environment
+        // if the .env.local file is missing or the variable is not set.
+        throw new Error("NEXT_PUBLIC_FIREBASE_API_KEY is not set in the environment.");
+    }
+
+    return {
+        "projectId": "studio-5398649656-398ca",
+        "appId": "1:558032123487:web:47872311f8d5284d45cdea",
+        "apiKey": apiKey,
+        "authDomain": "studio-5398649656-398ca.firebaseapp.com",
+        "measurementId": "",
+        "messagingSenderId": "558032123487"
+    };
 };
