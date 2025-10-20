408c5507a4bf2ddc07478f67f472292a44bcdc8e
Emmanuel TUYISHIMIRE <emmanueltuyishimire766@gmail.com>
2025-10-20 14:52:23 +0000

    is it my project ready for netlify deploy and hosting

---
+++ src/next.config.ts
@@ -0,0 +1,22 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  devIndicators: {
+    allowedDevOrigins: [
+      '*.cloudworkstations.dev',
+    ],
+  },
+};
+
+export default nextConfig;
