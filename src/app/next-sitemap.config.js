9922e3fafa055849523b1f0809fdea9b0f00a984
Emmanuel TUYISHIMIRE <emmanueltuyishimire766@gmail.com>
2025-10-26 16:05:50 +0000

    make my web app sitemap available to sitemap.xml file name

--- src/app/next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://www.maths.calculation.site',
-  generateRobotsTxt: true, // (optional)
-  // ...other options
-}
