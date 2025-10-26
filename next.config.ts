5f356a67b254e07667a7375bfdace3b270a17f4e
Emmanuel TUYISHIMIRE <emmanueltuyishimire766@gmail.com>
2025-10-26 07:45:35 +0000

    this confirms your next.config.ts file still has leftover Git conflict m

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
-<<<<<<< HEAD
   output: 'export',
-  basePath: '/mathcalculators',  // <--- Add this line
-=======
->>>>>>> 8ee1d91 (optimize LCP, render blocking, and other things to optimize app performa)
-=======
-  output: 'export',
->>>>>>> b8cdf3e (make it ready for netlify hosting.)
+  basePath: '/mathcalculators',
   images: {
     unoptimized: true,
   },
