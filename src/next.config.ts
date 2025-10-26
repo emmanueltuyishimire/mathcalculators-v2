c39934ef6237906353b1cbf43640b84745561b76
Emmanuel TUYISHIMIRE <emmanueltuyishimire766@gmail.com>
2025-10-26 15:25:14 +0000

    Use efficient cache lifetimes Est savings of 17 KiB A long cache lifetim

--- src/next.config.ts
+++ src/next.config.ts
@@ -2,9 +2,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  images: {
-    unoptimized: true,
-  },
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
